6568b007cc6029e589fcb37285932c265741d8c7
Maulana Abdul Siddiq <siddiqmaulanaabdul@gmail.com>
2018-02-21 17:43:47 +0700

    tambah index, operator, variabel

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "<h1>Selamat datang di Web Programming 1</h1>"
+
+?>
---
+++ operator.php
@@ -0,0 +1,18 @@
+<?php
+
+$a = 5;
+$b = 3;
+$c = 5;
+$d = "5";
+
+echo "$a + $b = "; echo $a + $b; echo "<br>";
+echo "$a / $b = "; echo $a / $b; echo "<br>";
+echo "$a > $b : "; echo $a > $b; echo "<br>";
+echo "$a == $c : "; echo $a == $c; echo "<br>";
+echo "$a == $d : "; echo $a == $d; echo "<br>";
+echo "$a === $d : "; echo $a === $d; echo "<br>";
+
+echo $a++; echo "<br>";
+echo ++$a; echo "<br>";
+
+?>
