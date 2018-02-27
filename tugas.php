584b1366b3b7af0cfd589b040aa57e006a87a96e
Maulana Abdul Siddiq <siddiqmaulanaabdul@gmail.com>
2018-02-27 21:50:28 +0700

    commit tugas

---
+++ tugas.php
@@ -0,0 +1,17 @@
+<?php
+
+$nama = "Maulana Abdul Siddiq";
+$tempat_lahir = "Jakarta";
+$tanggal_lahir = "23 Mei 1998";
+$usia = 20;
+$pekerjaan = "Mahasiswa";
+$alamat = "Jl. Cipto Mangunkusumo, Krandon, Tegal";
+
+echo "Nama          : " .$nama. "<br>";
+echo "Tempat lahir  : " .$tempat_lahir. "<br>";
+echo "Tanggal Lahir : " .$tanggal_lahir. "<br>";
+echo "Usia          : " .$usia. "<br>";
+echo "Pekerjaan     : " .$pekerjaan. "<br>";
+echo "Alamat        : " .$alamat;
+
+?>
