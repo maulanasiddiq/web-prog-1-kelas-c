4b83aa7c93549f364338ed7926a6ddfb4ccf9d5c
Maulana Abdul Siddiq <siddiqmaulanaabdul@gmail.com>
2018-03-07 16:29:05 +0700

    tambah praktek-3 if

--- praktek-3/coba-if.php
+++ praktek-3/coba-if.php
@@ -5,15 +5,22 @@
 
 <?php
 if(array_key_exists("kelas", $_GET)) {
-    switch($_GET["kelas"]) {
-        case "4a":
-            echo 'Kamu di kelas 4a kan.';
-            break;
-        case "4c":
-            echo "Kamu adalah mahasiswa terbaik";
-            break;
-        default :
-            echo "Kamu bukan mahasiswa semester 4";
+    // switch($_GET["kelas"]) {
+    //     case "4a":
+    //         echo 'Kamu di kelas 4a kan.';
+    //         break;
+    //     case "4c":
+    //         echo "Kamu adalah mahasiswa terbaik";
+    //         break;
+    //     default :
+    //         echo "Kamu bukan mahasiswa semester 4";
+    // }
+    if($_GET["kelas"] == "4a") {
+        echo "Kamu di kelas 4a kan.";
+    } elseif($_GET["kelas"] == "4c") {
+        echo "Kamu adalah mahasiswa terbaik.";
+    } else {
+        echo "Kamu bukan mahasiswa semester 4";
     }
 }
 ?>
