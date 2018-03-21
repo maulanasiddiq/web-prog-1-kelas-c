84cfbead1674a59406b46c193773bb7abe9d9c69
Maulana Abdul Siddiq <siddiqmaulanaabdul@gmail.com>
2018-03-21 16:56:34 +0700

    Tambah Project Stok-app

--- stok-app/simpan-baru.php
+++ stok-app/simpan-baru.php
@@ -1,16 +1,30 @@
 <?php
-$server = "localhost";
-$username = "root";
-$password = "";
-$db = "phb";
+include "koneksi.php";
 
-$koneksi = new mysqli($server, $username, $password, $db);
+$koneksiObj = new Koneksi();
+$koneksi = $koneksiObj->getKoneksi();
 
 if($koneksi->connect_error) {
     echo "Gagal koneksi : " . $koneksi->connect_error;
 } else {
     echo "Sambungan basis data berhasil";
 }
+
+// echo "KODE : " . $_POST["kode"];
+// echo "NAMA BARANG : " . $_POST["namaBarang"];
+// echo "STOK : " . $_POST["stok"];
+
+$query = "insert into stok_barang(kode, nama_barang, stok) " . "values(" . $_POST["kode"] . ",'" .
+        $_POST["namaBarang"] . "'," . $_POST["stok"] . ")";
+
+if($koneksi->query($query) === true) {
+    echo "<br> Data " . $_POST["namaBarang"] . " berhasil disimpan".
+    '<a href="main.php">Lihat Data</a>';
+} else {
+    echo "<br> Data GAGAL disimpan";
+}
+
+$koneksi->query($query);
 
 $koneksi->close();
 ?>
